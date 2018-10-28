bee67c3dd49609a4111c7917cb50a900d6ad7184
Leo <leo@Leos-MacBook-Pro.local>
2018-10-28 20:12:22 +0200

    routing

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var app = express();
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
 const connection = require('./mysql');
+const SECONDS_TOLERANCE = 1;
 
 
 const port = 3001
@@ -14,19 +15,23 @@ const CoinsLive = require('./exchanges/coinslive');
 app.get('/crawl', (req, res) => {
 	new CoinCap().crawl();
 	new CoinsLive().crawl();
-	getQuotes();
-	res.send('Crawling')
+	emitQuotes();
+	res.send('<script>setTimeout(() => location.reload(), 5000)</script>')
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  	emitQuotes();
+  	console.log('a user connected');
 });
 
 
-function getQuotes(){
+function emitQuotes(){
 	connection.query(`select cp.coin, round((cp2.price_usd + cp.price_usd)/2,2) as avg_price, cp.created_at from coin_prices as cp, coin_prices as cp2
-	where cp.coin = cp2.coin and cp.created_at = cp2.created_at and cp.source != cp2.source
-	order by created_at DESC limit 3`, function (error, results, fields) {
+						where cp.coin = cp2.coin and cp.created_at >= DATE_SUB(cp2.created_at, interval ${SECONDS_TOLERANCE} second) and 
+						cp.created_at <= DATE_ADD(cp2.created_at, interval ${SECONDS_TOLERANCE} second) and cp.source != cp2.source
+						order by created_at DESC limit 3
+
+`, function (error, results, fields) {
 		io.emit('price_change', {results});
 	});	
 }
