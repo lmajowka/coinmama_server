34a60a17da9d68b299ac3c6b53cb0a5468a7cfd3
Leo <leo@Leos-MacBook-Pro.local>
2018-10-28 21:29:19 +0200

    add coin quote page

--- app.js
+++ app.js
@@ -19,6 +19,20 @@ app.get('/crawl', (req, res) => {
 	res.send('<script>setTimeout(() => location.reload(), 5000)</script>')
 });
 
+app.get('/coin/:name', function(req , res){
+
+	connection.query(`select cp.coin, round((cp2.price_usd + cp.price_usd)/2,2) as avg_price, cp.created_at from coin_prices as cp, coin_prices as cp2
+						where cp.coin = cp2.coin and cp.created_at >= DATE_SUB(cp2.created_at, interval ${SECONDS_TOLERANCE} second) and 
+						cp.created_at <= DATE_ADD(cp2.created_at, interval ${SECONDS_TOLERANCE} second) and cp.source != cp2.source
+						and cp.coin = '${req.params.name}'
+						group by 1,2,3
+						order by created_at DESC LIMIT 20`, function (error, results, fields) {
+							res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+							res.json({"items": results});
+						});
+ 
+});
+
 io.on('connection', function(socket){
   	emitQuotes();
   	console.log('a user connected');
